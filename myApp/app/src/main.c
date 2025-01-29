b58aa0413c7267ebdb05318838d09065ca037436
junpinfoo <foojunpin@gmail.com>
2025-01-28 17:47:56 -0800

    joystick

--- myApp/app/src/main.c
+++ myApp/app/src/main.c
@@ -4,12 +4,26 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "hal/led.h"
+#include "hal/joystick.h"
 
 int main() {
     // Example usage
     // set_trigger(&leds[0], "heartbeat"); // Set ACT LED trigger to "heartbeat"
-    set_brightness(&leds[0], 1);        // Set ACT LED brightness to 1
-    set_brightness(&leds[0], 0);        // Set ACT LED brightness to 1
-    set_brightness(&leds[1], 1);        // Set ACT LED brightness to 1
+    // set_brightness(&leds[0], 1);        // Set ACT LED brightness to 1
+    // set_brightness(&leds[0], 0);        // Set ACT LED brightness to 1
+    // set_brightness(&leds[1], 1);        // Set ACT LED brightness to 1
+
+    printf("Initializing Joystick...\n");
+    Joystick_initialize();
+    printf("Joystick initialized successfully.\n");
+
+    while (1) {
+        // Get the joystick reading
+        struct JoystickData data = Joystick_getReading();
+
+        // Print the joystick data
+        printf("Joystick Data - X: %.2f, Y: %.2f, IsPressed: %s\n",
+               data.x, data.y, data.isPressed ? "true" : "false");
+    }
 
 }
---
+++ myApp/hal/src/i2c.c
@@ -0,0 +1,54 @@
+#include "hal/i2c.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
+int init_i2c_bus(const char* bus, int address) {
+    int i2c_file_desc = open(bus, O_RDWR);
+    if (i2c_file_desc == -1) {
+        printf("I2C DRV: Unable to open bus for read/write (%s)\n", bus);
+        perror("Error is:");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ioctl(i2c_file_desc, I2C_SLAVE, address) == -1) {
+        perror("Unable to set I2C device to slave address.");
+        exit(EXIT_FAILURE);
+    }
+
+    return i2c_file_desc;
+}
+
+void write_i2c_reg16(int i2c_file_desc, uint8_t reg_addr, uint16_t value) {
+    int tx_size = 1 + sizeof(value);
+    uint8_t buff[tx_size];
+    buff[0] = reg_addr;
+    buff[1] = (value & 0xFF);
+    buff[2] = (value & 0xFF00) >> 8;
+
+    int bytes_written = write(i2c_file_desc, buff, tx_size);
+    if (bytes_written != tx_size) {
+        perror("Unable to write i2c register");
+        exit(EXIT_FAILURE);
+    }
+}
+
+uint16_t read_i2c_reg16(int i2c_file_desc, uint8_t reg_addr) {
+    int bytes_written = write(i2c_file_desc, &reg_addr, sizeof(reg_addr));
+    if (bytes_written != sizeof(reg_addr)) {
+        perror("Unable to write i2c register.");
+        exit(EXIT_FAILURE);
+    }
+
+    uint16_t value = 0;
+    int bytes_read = read(i2c_file_desc, &value, sizeof(value));
+    if (bytes_read != sizeof(value)) {
+        perror("Unable to read i2c register");
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
